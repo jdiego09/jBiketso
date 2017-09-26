35e823f340756a4981db1964a021badfff13b546
jcalvo <jcalvo@CopInf12.coopeagri.local>
2017-09-26 12:45:56 -0600

    prueba de Commit desde netbeans

--- src/jbiketso/MainApp.java
+++ src/jbiketso/MainApp.java
@@ -2,11 +2,7 @@
 package jbiketso;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-import jbiketso.controller.LoginController;
 import jbiketso.utils.AppWindowController;
 import jbiketso.utils.Parametros;
 //hola
