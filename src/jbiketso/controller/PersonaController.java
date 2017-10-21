1e04684784d01a51141624aa033ba843d928b00a
Luis Diego <>
2017-10-20 22:50:15 -0600

    Commit de persona

--- src/jbiketso/controller/PersonaController.java
+++ src/jbiketso/controller/PersonaController.java
@@ -5,9 +5,16 @@
  */
 package jbiketso.controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import jbiketso.model.entities.BikPersona;
 
 /**
  *
@@ -15,9 +22,40 @@ import javafx.fxml.Initializable;
  */
 public class PersonaController implements Initializable{
 
+    @FXML
+    private JFXTextField txtCedula;
+    
+    @FXML
+    private JFXTextField txtNombres;
+    
+    @FXML
+    private JFXTextField txtPrimerApellido;
+    
+    @FXML
+    private JFXTextField txtSegundoApellido;
+    
+    @FXML
+    private JFXComboBox cmbGenero;
+    
+    @FXML
+    private JFXTextField txtNacionalidad;
+    
+    @FXML
+    private JFXComboBox cmbEstadoCivil;
+    
+    @FXML
+    private JFXTextField txtProfesion;
+    
+    @FXML
+    private DatePicker dtpFechaNacimiento;
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+  
+    @FXML
+    private void guardarPersona(ActionEvent event){
+        BikPersona persona;
+    }
 }
