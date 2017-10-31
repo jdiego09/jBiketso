2432b63e54b2def3848543e50815107e86f3cc24
jcalvo <jcalvo@CopInf12.coopeagri.local>
2017-10-31 17:25:50 -0600

    navegacion de pantallas

--- src/jbiketso/controller/InicioController_1.java
+++ src/jbiketso/controller/InicioController_1.java
@@ -7,25 +7,21 @@ package jbiketso.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
  *
- * @author esanchez
+ * @author jcalvo
  */
 public class InicioController implements Initializable {
 
-    @FXML
-    AnchorPane root;
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }   
-    
+   /**
+    * Initializes the controller class.
+    */
+   @Override
+   public void initialize(URL url, ResourceBundle rb) {
+      // TODO
+   }   
+   
 }
