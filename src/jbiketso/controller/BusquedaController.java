d8b698b71a2b143bc34983ea074693574a8fe0e0
jcalvo <jcalvo@CopInf12.coopeagri.local>
2017-11-07 17:41:28 -0600

    navegacion de pantallas

--- src/jbiketso/controller/BusquedaController.java
+++ src/jbiketso/controller/BusquedaController.java
@@ -9,7 +9,10 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -18,7 +21,12 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
@@ -147,5 +155,72 @@ public class BusquedaController implements Initializable {
       vbxCriterios.getChildren().add(jbtn);
 
    }
+   /*
+   public void busquedaSorteo() {
+        try {
+
+            lblTitulo.setText("Búsqueda de Sorteos");
+            Label lblCompania = new Label("Compañia");
+            ComboBox cmbCompania = new ComboBox();
+            cmbCompania.setOnKeyPressed(keyEnter);
+            cmbCompania.setItems(cargarCompanias());
+            cmbCompania.getSelectionModel().selectFirst();
+            Label lblCodigo = new Label("Código");
+            TextField txfCodigo = new TextField();
+            txfCodigo.setOnKeyPressed(keyEnter);
+            Label lblDescripcion = new Label("Descripción");
+            TextField txfDescripcion = new TextField();
+            txfDescripcion.setOnKeyPressed(keyEnter);
+
+            vbxParametros.getChildren().clear();
+            vbxParametros.getChildren().add(lblCompania);
+            vbxParametros.getChildren().add(cmbCompania);
+            vbxParametros.getChildren().add(lblCodigo);
+            vbxParametros.getChildren().add(txfCodigo);
+            vbxParametros.getChildren().add(lblDescripcion);
+            vbxParametros.getChildren().add(txfDescripcion);
+
+            tbvBusqueda.getColumns().clear();
+            tbvBusqueda.getItems().clear();
+            TableColumn<SorteoDto, CompaniaDto> tbcCompania = new TableColumn<>("Compania");
+            tbcCompania.setPrefWidth(100);
+            tbcCompania.setCellValueFactory(cd -> cd.getValue().compania);
+            TableColumn<SorteoDto, String> tbcCodigo = new TableColumn<>("Código");
+            tbcCodigo.setPrefWidth(100);
+            tbcCodigo.setCellValueFactory(cd -> cd.getValue().codigo);
+            TableColumn<SorteoDto, String> tbcDescripcion = new TableColumn<>("Descripción");
+            tbcDescripcion.setPrefWidth(300);
+            tbcDescripcion.setCellValueFactory(cd -> cd.getValue().descripcion);
+
+            tbvBusqueda.getColumns().add(tbcCompania);
+            tbvBusqueda.getColumns().add(tbcCodigo);
+            tbvBusqueda.getColumns().add(tbcDescripcion);
+            tbvBusqueda.refresh();
+
+            btnFiltrar.setOnAction((ActionEvent event) -> {
+                tbvBusqueda.getItems().clear();
+                SorteoService service = (SorteoService) AppContext.getInstance().get("SorteoService");
+                String compania = "%%";
+                if (cmbCompania.getSelectionModel().getSelectedItem() != null) {
+                    compania = ((CompaniaDto) cmbCompania.getSelectionModel().getSelectedItem()).getCodigo();
+                }
+                String codigo = "%" + txfCodigo.getText() + "%";
+                String descripcion = "%" + txfDescripcion.getText() + "%";
+
+                Respuesta respuesta = service.getSorteos(compania, codigo, descripcion);
+
+                if (respuesta.getEstado()) {
+                    ObservableList<SorteoDto> sorteos = FXCollections.observableList((List<SorteoDto>) respuesta.getResultado("Sorteos"));
+                    tbvBusqueda.setItems(sorteos);
+                    tbvBusqueda.refresh();
+                } else {
+                    Mensaje.getInstance().showModal(Alert.AlertType.ERROR, "Consultar sorteos", getStage(), respuesta.getMensaje());
+                }
+            });
+        } catch (Exception ex) {
+            Logger.getLogger(BusquedaController.class.getName()).log(Level.SEVERE, "Error consultando los sorteos.", ex);
+            Mensaje.getInstance().showModal(Alert.AlertType.ERROR, "Consultar sorteos", getStage(), "Ocurrió un error consultado los sorteos.");
+        }
+    }*/
 
 }
